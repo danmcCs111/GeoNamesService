f85662e10d1f1dc249c7e20c765dd4e16447f43f
danmcCs111 <danmccs111@gmail.com>
2026-07-28 15:40:48 -0400

    update multi-threaded script to accept a list of drives/paths

--- multi-thread_createInsert.sh
+++ multi-thread_createInsert.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 
-if [ "$#" -ne 3 ]; then
-	echo "need to provide processing file and output filename"
+if [ "$#" -ne 4 ]; then
+	echo "provide: "
+	echo "csv"
+	echo "output sql filename"
+	echo "number of file splits"
+	echo "drive path locations, '@' delimited"
 	exit
 fi
 
 fle=$1
 outFle=$2
 count=$3
+drives=$4
+drives=( `echo $drives | sed 's/@/ /g'` )
 
 
 len=$(( `wc -l $fle | egrep -o "[0-9]+"` ))
@@ -17,6 +23,8 @@ echo $len
 it=$(( $len / $count ))
 start=1
 echo $it
+drivesCount=${#drives[@]}
+dCounter=0
 
 for t in $(eval echo "{1..$count}")
 do
@@ -27,8 +35,15 @@ do
 		end=$(( $t * $it ))
 	fi
 	#processing.
-	cat $fle | sed -n "${start}","${end}"p > $fle-$t.csv
+	path="${drives[$dCounter]}/"
+	cat $fle | sed -n "${start}","${end}"p > $path$fle-$t.csv
 	start=$(( $end + 1))
 
-	./createInsert.sh $fle-$t.csv $outFle-$t.sql&
+	./createInsert.sh $path$fle-$t.csv $path$outFle-$t.sql&
+
+	dCounter=$(( $dCounter + 1 ))
+	if [ $dCounter -eq $drivesCount ]
+	then
+		dCounter=0
+	fi
 done
